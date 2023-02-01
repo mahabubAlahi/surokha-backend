588d51666a5466d2e329a75920165b915035b921
mahabubAlahi <mahabubalahi9531@gmail.com>
2023-02-01 21:46:06 +0600

    refactor: add verifytoken middleware in admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,9 +9,9 @@ const vaccinationController = require('../controllers/vaccinationController');
 const BASE_PREFIX = "/api";
 
 router.route("/sigin").post(authMiddleware.verifySignInRequestObject, userController.signin);
-router.route("/create-vaccination").post(vaccinationMiddleware.verifyVaccinationRequestObject, vaccinationController.createVaccinationDetails);
-router.route("/get-all-vaccination").get(vaccinationController.getAllVaccinationDetails);
-router.route("/get-vaccination").get(vaccinationController.getVaccinationDetailsById);
+router.route("/create-vaccination").post(authMiddleware.verifyToken, vaccinationMiddleware.verifyVaccinationRequestObject, vaccinationController.createVaccinationDetails);
+router.route("/get-all-vaccination").get(authMiddleware.verifyToken, vaccinationController.getAllVaccinationDetails);
+router.route("/get-vaccination").get(authMiddleware.verifyToken, vaccinationController.getVaccinationDetailsById);
 
 module.exports = {
     router,
