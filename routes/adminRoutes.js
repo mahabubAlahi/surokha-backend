06b6b171f018850eeefee862826654f6c24f3319
mahabubAlahi <mahabubalahi9531@gmail.com>
2023-02-01 21:25:23 +0600

    feat: add admin routes
    - sigin
    - create-vaccination
    - get-all-vaccination
    - get-vaccination

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const testController = require("../controllers/test-controller");
+const userController = require("../controllers/userController");
+const authMiddleware = require('../middleware/authMiddleware');
+const vaccinationMiddleware = require('../middleware/vaccinationMiddleware');
+const vaccinationController = require('../controllers/vaccinationController');
 
 const BASE_PREFIX = "/api";
 
-router.route("/test").get(testController.test);
+router.route("/sigin").post(authMiddleware.verifySignInRequestObject, userController.signin);
+router.route("/create-vaccination").post(vaccinationMiddleware.verifyVaccinationRequestObject, vaccinationController.createVaccinationDetails);
+router.route("/get-all-vaccination").get(vaccinationController.getAllVaccinationDetails);
+router.route("/get-vaccination").get(vaccinationController.getVaccinationDetailsById);
 
 module.exports = {
     router,
