7878c573f2823dc91fba56956954f34022f60715
mahabubAlahi <mahabubalahi9531@gmail.com>
2023-02-01 21:45:43 +0600

    feat: add `verifyToken` middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,6 @@
 const userService = require('../services/user-service');
+const config = require('../config/auth.config');
+const jwt = require("jsonwebtoken");
 
 const verifySignInRequestObject = async (req, res, next) => {
     try {
@@ -13,6 +15,26 @@ const verifySignInRequestObject = async (req, res, next) => {
     }
 }
 
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'] || req.headers['Authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(403).send({
+            status: false,
+            msg: "Token not found"
+        });
+    }
+    jwt.verify(token, config.accessTokenSecret, (err, decoded) => {
+        if (err) return res.status(401).send({
+            status: false,
+            msg: err
+        });
+        next();
+    });
+};
+
 module.exports = {
-    verifySignInRequestObject
+    verifySignInRequestObject,
+    verifyToken
 }
